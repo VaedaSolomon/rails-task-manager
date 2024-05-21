4bfa82ab90e8b8e7993616c1ab053126f7a30799
Vaeda Solomon <vaeda.solomon@gmail.com>
2024-05-21 14:32:19 -0400

    completed addition of new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,23 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to tasks_path
+  end
+
+
+  private
+
+  # This is a strong params. Can also name it strong_params
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
