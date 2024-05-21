60b471c9a51a040a66657befd5c09725d57a6b67
Vaeda Solomon <vaeda.solomon@gmail.com>
2024-05-21 15:21:01 -0400

    completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :find_task, only:[:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   def new
@@ -19,18 +21,28 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     @task.update(task_params)
+
+    redirect_to tasks_path
+  end
+
+  def destroy
+    # @task = Task.find(params[:id])
+    @task.destroy
 
     redirect_to tasks_path
   end
 
   private
 
+  def find_task
+    @task = Task.find(params[:id])
+  end
   # This is a strong params. Can also name it strong_params
   def task_params
     params.require(:task).permit(:title, :details)
--- config/routes.rb
+++ config/routes.rb
@@ -8,16 +8,21 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
+  # list all tasks
   get 'tasks', to: 'tasks#index', as: :tasks
 
+  # details of 1 task
   get 'tasks/new', to: 'tasks#new', as: :new
-
   get 'tasks/:id', to: 'tasks#show', as: :task
 
+  # create new task (new has to be above id before or else prog will break)
   post 'tasks', to: 'tasks#create'
 
+  # edit task
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
-
   patch 'tasks/:id', to: 'tasks#update'
+
+  # delete task
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete
 
 end
